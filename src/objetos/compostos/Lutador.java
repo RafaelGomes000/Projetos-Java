3d272dc35235e04ae7dad876d798d3c14ec85a1c
Rafael Almeida <Rafael Almeida@ACT-4534.altransp.com.br>
2023-01-10 11:51:29 -0300

    Adicionando Atributos

---
+++ src/objetos/compostos/Lutador.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package objetos.compostos;
+
+/**
+ *
+ * @author Rafael Almeida
+ */
+public class Lutador {
+    //Atributos
+    private String nome;
+    private String nacionalidade;
+    private int idade;
+    private float altura;
+    private float peso;
+    private String categoria;
+    private double vitoria;
+    private double derrota;
+    private double empate;
+    //Métodos Públicos
+    
+    //Métodos Especiais
+}
