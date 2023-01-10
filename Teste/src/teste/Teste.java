669a97359aca86a14cc28fd97a11818505c73715
Rafael Almeida <Rafael Almeida@ACT-4534.altransp.com.br>
2023-01-10 10:55:33 -0300

    Criando projeto

--- Teste/src/teste/Teste.java
+++ Teste/src/teste/Teste.java
@@ -4,6 +4,8 @@
  */
 package teste;
 
+
+
 /**
  *
  * @author Ronaldo Total 90
@@ -14,7 +16,7 @@ public class Teste {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
     }
     
 }
