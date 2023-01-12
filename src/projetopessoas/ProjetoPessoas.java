53d9525822a9030d96103f2b2226305e69d872ec
Rafael Almeida <Rafael Almeida@ACT-4534.altransp.com.br>
2023-01-12 11:00:47 -0300

    Ceiando herança

--- src/projetopessoas/ProjetoPessoas.java
+++ src/projetopessoas/ProjetoPessoas.java
@@ -1,20 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package projetopessoas;
 
-/**
- *
- * @author Rafael Almeida
- */
 public class ProjetoPessoas {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+      
+        Pessoa p1 = new Pessoa();
+        Aluno p2 = new Aluno();
+        Professor p3 = new Professor();
+        Funcionario p4 = new Funcionario();
+        
+        p1.setNome("Pedro");
+        p2.setNome("Maria");
+        p3.setNome("Claudio");
+        p4.setNome("Fabiana");
+        
+        p2.setCurso("Informática");
+        p3.setSalario(2500.7f);
+        p4.setSetor("Estoque");
+        
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+        System.out.println(p3.toString());
+        System.out.println(p4.toString());
     }
     
 }
